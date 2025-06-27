04b5b9df025a2cd21f8457f0276437366236f52f
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-06-27 09:42:08 +0100

    Updated animate on scroll effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,42 @@
+"use client";
 import DemoSection from "@/components/home/demo-section";
+import { motion, useScroll, useTransform } from "motion/react";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function HomePage() {
+  const parentRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: parentRef,
+    offset: ["start start", "end end"],
+  });
+
+  // Animate opacity and y based on scroll
+  const right = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const rightFast = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["0%", "500px", "100%"]
+  );
+  const left = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["0%", "-500px", "-100%"]
+  );
+  const up = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["0%", "-500px", "-100%"]
+  );
+  const down = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["0%", "500px", "100%"]
+  );
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0.1, 0]);
   return (
-    <>
-      <section className="max-w-7xl mx-auto p-5 snap-start">
+    <main ref={parentRef}>
+      <section className="max-w-7xl mx-auto p-5 sticky top-0 overflow-x-hidden">
         {/* hero section */}
         <div className="h-screen  relative">
           <div className="space-y-4 mb-12">
@@ -17,38 +49,57 @@ export default function HomePage() {
               vision.
             </p>
             <div className="flex gap-3">
-              <button
-                type="button"
-                className="bg-green-900 text-white py-1 px-4 rounded-3xl"
-              >
-                Get in Early
-              </button>
-              <button
-                type="button"
-                className="text-green-900 bg-white py-1 px-4 rounded-3xl border-2 border-green-900"
-              >
-                How it works
-              </button>
+              <motion.div style={{ x: rightFast, opacity }}>
+                <button
+                  type="button"
+                  className="bg-green-900 text-white py-1.5 px-4 rounded-3xl"
+                >
+                  Get in Early
+                </button>
+              </motion.div>
+              <motion.div style={{ x: left, y: down, opacity }}>
+                <button
+                  type="button"
+                  className="text-green-900 bg-white py-1 px-4 rounded-3xl border-2 border-green-900"
+                >
+                  How it works
+                </button>
+              </motion.div>
             </div>
           </div>
           {/* Shapes */}
           <div className="flex gap-6 items-end absolute w-full top-[10%]">
-            <div className="inline-block h-[250px] w-[150px] rounded-r-[80px] border-8 border-gray-400" />
+            <motion.div
+              style={{ x: left, opacity }}
+              className="inline-block h-[250px] w-[150px] rounded-r-[80px] border-8 border-gray-400"
+            />
             <div className="flex flex-col gap-6 flex-grow">
               <div className="flex gap-6 items-end">
-                <div className="inline-block h-[80px] w-[150px] rounded-t-full bg-pink border-8 bg-[#FDE3FA] border-[#f5c9f0]" />
-                <div className="inline-block bg-green-700 border-8 border-green-900 rounded-4xl rounded-tl-none h-[50px] w-[200px]" />
-                <div className="inline-block bg-[#7EF1EA] min-w-[500px] flex-grow h-[500px] rounded-tl-[100px] border-8 border-[#097a73]" />
+                <motion.div
+                  style={{ y: up, opacity }}
+                  className="flex gap-6 items-end"
+                >
+                  <div className="inline-block h-[80px] w-[150px] rounded-t-full bg-pink border-8 bg-[#FDE3FA] border-[#f5c9f0]" />
+                  <div className="inline-block bg-green-700 border-8 border-green-900 rounded-4xl rounded-tl-none h-[50px] w-[200px]" />
+                </motion.div>
+                <motion.div
+                  style={{ x: right, opacity }}
+                  className="inline-block bg-[#7EF1EA] min-w-[500px] flex-grow h-[500px] rounded-tl-[100px] border-8 border-[#097a73]"
+                />
               </div>
-              <div className="inline-block w-full bg-yellow-300 border-8 border-yellow-700 h-[80px] rounded-bl-[300px] rounded-r-[150px]" />
+              <motion.div
+                style={{ x: right, opacity }}
+                className="inline-block w-full bg-yellow-300 border-8 border-yellow-700 h-[80px] rounded-bl-[300px] rounded-r-[150px]"
+              />
             </div>
           </div>
         </div>
       </section>
+      <div className="h-[150vh]" />
+      <DemoSection ref={parentRef} />
+      <div className="h-[800vh]" />
 
-      <DemoSection />
-
-      <section className="max-w-7xl mx-auto relative p-5 my-12 overflow-hidden snap-start">
+      <section className="max-w-7xl mx-auto bg-white z-30 sticky top-0 p-5 my-12 overflow-hidden">
         <div className="absolute top-14 left-1/2 w-[300px] transform -translate-x-1/2 text-center space-y-4">
           <h2 className="text-xl">The Platform</h2>
           <h3 className="text-4xl font-semibold uppercase">
@@ -65,8 +116,8 @@ export default function HomePage() {
             Get in Early
           </button>
         </div>
-        <div className="overflow-auto h-screen p-3 snap-y snap-mandatory">
-          <div className="flex justify-between h-screen snap-start">
+        <div className="overflow-auto h-screen p-3">
+          <div className="flex justify-between h-screen">
             <Image
               src="/example.png"
               alt="example"
@@ -82,7 +133,7 @@ export default function HomePage() {
               className="rounded-4xl object-cover h-[70%] mt-auto"
             />
           </div>
-          <div className="flex justify-between h-screen snap-start">
+          <div className="flex justify-between h-screen">
             <Image
               src="/example.png"
               alt="example"
@@ -98,7 +149,7 @@ export default function HomePage() {
               className="rounded-4xl object-cover h-[70%] mt-auto"
             />
           </div>
-          <div className="flex justify-between h-screen snap-start">
+          <div className="flex justify-between h-screen">
             <Image
               src="/example.png"
               alt="example"
@@ -114,7 +165,7 @@ export default function HomePage() {
               className="rounded-4xl object-cover h-[70%] mt-auto"
             />
           </div>
-          <div className="flex justify-between h-screen snap-start">
+          <div className="flex justify-between h-screen">
             <Image
               src="/example.png"
               alt="example"
@@ -132,6 +183,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-    </>
+    </main>
   );
 }
--- components/home/demo-section.tsx
+++ components/home/demo-section.tsx
@@ -1,54 +1,79 @@
 "use client";
-import { motion, useInView } from "motion/react";
+import { motion, useInView, useScroll, useTransform } from "motion/react";
 import Image from "next/image";
-import { useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const DemoSection = () => {
-  const parentRef = useRef(null);
+const DemoSection = forwardRef<HTMLElement>((props, ref) => {
+  const parentRef = useRef<HTMLElement>(null);
+
+  // Expose parentRef to parent via the forwarded ref
+  useImperativeHandle(ref, () => parentRef.current as HTMLElement);
+
   const isInView = useInView(parentRef, { once: true, amount: 0.5 });
   console.log(isInView);
+  const { scrollYProgress } = useScroll({
+    target: parentRef,
+    offset: ["start end", "end start"],
+  });
+
+  // Animate opacity and y based on scroll
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0, 0.9, 1.4]);
+  const radius = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    ["100%", "20%", "-10%"]
+  );
+  // const right = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  // const left = useTransform(
+  //   scrollYProgress,
+  //   [0, 0.5, 1],
+  //   ["0%", "-500px", "-100%"]
+  // );
+  // const up = useTransform(
+  //   scrollYProgress,
+  //   [0, 0.5, 1],
+  //   ["0%", "-500px", "-100%"]
+  // );
+  const down = useTransform(scrollYProgress, [0, 1], ["-100%", "80%"]);
   return (
-    <section
-      ref={parentRef}
-      className="max-w-7xl relative overflow-hidden mx-auto snap-start"
-    >
-      <motion.div
-        style={{
-          position: "absolute",
-          left: "50%",
-          transform: "translateX(-50%)",
-        }}
-        initial={{ top: "-100%" }}
-        animate={isInView ? { top: "8%" } : { top: "-100%" }}
-        transition={{ delay: 2, duration: 1, ease: "easeInOut" }}
-      >
-        <Image
-          src="/demo-phone.png"
-          alt="Demo Image"
-          width={500}
-          height={500}
+    <section className="max-w-7xl sticky top-0 z-30 overflow-hidden mx-auto bg-white">
+      <div className="relative">
+        <motion.div
+          className="absolute left-1/2 -translate-x-1/2 z-50"
+          style={{ y: down }}
+          // initial={{ top: "-100%" }}
+          // animate={isInView ? { top: "8%" } : { top: "-100%" }}
+          transition={{ ease: "easeInOut" }}
+        >
+          <Image
+            src="/demo-phone.png"
+            alt="Demo Image"
+            width={500}
+            height={500}
+          />
+        </motion.div>
+        <motion.div
+          style={{
+            scale: scale,
+            // opacity: opacity,
+            borderTopRightRadius: radius,
+            borderTopLeftRadius: radius,
+            originX: 0.5,
+            originY: 1,
+          }}
+          // whileInView={{
+          //   scale: 1,
+          //   opacity: 1,
+          //   borderTopRightRadius: "0%",
+          //   borderTopLeftRadius: "0%",
+          //   transition: { duration: 1, ease: "easeInOut" },
+          // }}
+          // viewport={{ once: true }}
+          className="bg-[url(/demo-background.png)] h-screen w-full bg-cover"
         />
-      </motion.div>
-      <motion.div
-        initial={{
-          scale: 0,
-          opacity: 0,
-          borderTopRightRadius: "150%",
-          borderTopLeftRadius: "150%",
-          originX: 0.5,
-          originY: 1,
-        }}
-        whileInView={{
-          scale: 1,
-          opacity: 1,
-          borderTopRightRadius: "0%",
-          borderTopLeftRadius: "0%",
-          transition: { duration: 1, ease: "easeInOut" },
-        }}
-        viewport={{ once: true }}
-        className="bg-[url(/demo-background.png)] h-screen w-full bg-cover"
-      />
+      </div>
     </section>
   );
-};
+});
 export default DemoSection;
+DemoSection.displayName = "DemoSection";
